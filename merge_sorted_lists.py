b307fae0256bff3638950824f374f1ccc102af72
pinocchio-life-like <icbr19fl@gmail.com>
2023-11-30 10:13:24 +0300

    leet problems

---
+++ merge_sorted_lists.py
@@ -0,0 +1,25 @@
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def mergeTwoLists(l1, l2):
+    dummy = ListNode(0)
+    curr = dummy
+    
+    while l1 and l2:
+        if l1.val < l2.val:
+            curr.next = l1
+            l1 = l1.next
+        else:
+            curr.next = l2
+            l2 = l2.next
+        curr = curr.next
+    
+    if l1:
+        curr.next = l1
+    else:
+        curr.next = l2
+    
+    return dummy.next
