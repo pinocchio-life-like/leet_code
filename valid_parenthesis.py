971eb6f66de8950f6662d6778f9ee778e1398eab
pinocchio-life-like <icbr19fl@gmail.com>
2023-11-30 10:07:42 +0300

    leet problems

---
+++ valid_parenthesis.py
@@ -0,0 +1,11 @@
+def isValid(s):
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+    for char in s:
+        if char in mapping:
+            if not stack or stack.pop() != mapping[char]:
+                return False
+        else:
+            stack.append(char)
+    return not stack
+
