93857d8fcde2d4da85baa0537775dd5a8df36ff6
pinocchio-life-like <icbr19fl@gmail.com>
2023-12-02 08:43:42 +0300

    leet problems

---
+++ max_confusion.py
@@ -0,0 +1,17 @@
+def maxConsecutiveAnswers(answerKey: str, k: int) -> int:
+    max_consecutive = 0
+    max_count = 0
+    count = {'T': 0, 'F': 0}
+    left = 0
+
+    for right in range(len(answerKey)):
+        count[answerKey[right]] += 1
+        max_count = max(max_count, count[answerKey[right]])
+
+        if (right - left + 1) - max_count > k:
+            count[answerKey[left]] -= 1
+            left += 1
+
+        max_consecutive = max(max_consecutive, right - left + 1)
+
+    return max_consecutive
