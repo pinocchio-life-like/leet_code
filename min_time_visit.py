f8328fa32d34252019a745dc689d9244dafe5aee
pinocchio-life-like <icbr19fl@gmail.com>
2023-12-06 15:28:03 +0300

    leet problems

--- min_time_visit.py
+++ min_time_visit.py
@@ -3,5 +3,5 @@ def minTimeToVisitAllPoints(points):
     for i in range(len(points) - 1):
         x1, y1 = points[i]
         x2, y2 = points[i+1]
-        total_time += max(abs(x2 - x1), abs(y2 - y1))
+        total_time = total_time + max(abs(x2 - x1), abs(y2 - y1))
     return total_time
