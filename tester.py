109951d0ea74bd20ab1e9c514dd2e292efe43900
pinocchio-life-like <icbr19fl@gmail.com>
2023-12-09 18:08:05 +0300

    leet problems

--- tester.py
+++ tester.py
@@ -10,3 +10,22 @@
 haystack = "haystack"
 stack = "stack"
 print(haystack.index(stack))
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        result = []
+        self.inorderHelper(root, result)
+        return result
+    
+    def inorderHelper(self, node: TreeNode, result: List[int]):
+        if node:
+            self.inorderHelper(node.left, result)
+            result.append(node.val)
+            self.inorderHelper(node.right, result)
