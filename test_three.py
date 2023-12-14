94575cfcdea93b7be3fd39e4d111af619df3d60f
pinocchio-life-like <icbr19fl@gmail.com>
2023-12-14 08:14:49 +0300

    leet problems

--- test_three.py
+++ test_three.py
@@ -1,4 +1,20 @@
+def diffMatrix(grid):
+    m, n = len(grid), len(grid[0])
+    onesRow, onesCol = [0]*m, [0]*n
+    zerosRow, zerosCol = [0]*m, [0]*n
 
-num = [1,2,3,4]
-for i in num[::-1]:
-    print(i)
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == 1:
+                onesRow[i] += 1
+                onesCol[j] += 1
+            else:
+                zerosRow[i] += 1
+                zerosCol[j] += 1
+
+    diff = [[0]*n for _ in range(m)]
+    for i in range(m):
+        for j in range(n):
+            diff[i][j] = onesRow[i] + onesCol[j] - zerosRow[i] - zerosCol[j]
+
+    return diff
