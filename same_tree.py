e846f3338be048b4926f9791c3fbbe410ea612ef
pinocchio-life-like <icbr19fl@gmail.com>
2023-12-14 09:49:56 +0300

    leet problems

---
+++ same_tree.py
@@ -0,0 +1,15 @@
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def isSameTree(p, q):
+    if not p and not q:
+        return True
+    if not p or not q:
+        return False
+    if p.val != q.val:
+        return False
+    return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
