7c4b5b8c05ba1ed48fb24c428330b1ce399baa89
pinocchio-life-like <icbr19fl@gmail.com>
2023-12-20 16:07:04 +0300

    leet problems

---
+++ buy_chocolate.py
@@ -0,0 +1,15 @@
+def buyChoco(self, prices, money):
+    prices.sort()
+    left = 0
+    right = len(prices) - 1
+    
+    while left < right:
+        total = prices[left] + prices[right]
+        if total <= money:
+            return money - total
+        elif total > money:
+            right -= 1
+        else:
+            left += 1
+    
+    return money
