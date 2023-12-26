2d92deafe3e560e535c840da27b362c3608d2a5c
pinocchio-life-like <icbr19fl@gmail.com>
2023-12-26 16:11:56 +0300

    leet problems

--- solutions.py
+++ solutions.py
@@ -162,3 +162,28 @@ def postorderTraversal(root):
 
     return output
 
+def searchRange(nums, target):
+    def findFirst(nums, target):
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        return left if nums[left] == target else -1
+
+    def findLast(nums, target):
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2 + 1
+            if nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid
+        return left if nums[left] == target else -1
+
+    first, last = findFirst(nums, target), findLast(nums, target)
+    return [first, last] if first != -1 else [-1, -1]
+
+
