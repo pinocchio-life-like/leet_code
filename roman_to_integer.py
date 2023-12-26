f015791694da6d106597b565e6abb1bb272e8c1f
pinocchio-life-like <icbr19fl@gmail.com>
2023-12-26 11:21:49 +0300

    leet problems

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -2,8 +2,8 @@ def roman_to_integer(s):
     roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     prev_value = 0
-    
     for i in range(len(s)-1, -1, -1):
+        
         curr_value = roman_values[s[i]]
         
         if curr_value >= prev_value:
