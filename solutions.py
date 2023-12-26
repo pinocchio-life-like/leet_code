488afbab614474497c25b095a867f3f054782f6c
pinocchio-life-like <icbr19fl@gmail.com>
2023-12-26 16:06:41 +0300

    leet problems

--- solutions.py
+++ solutions.py
@@ -116,3 +116,26 @@ def hasCycle(head):
 
     return True
 
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+def preorderTraversal(root):
+    if root is None:
+        return []
+
+    stack, output = [root, ], []
+
+    while stack:
+        root = stack.pop()
+        if root is not None:
+            output.append(root.val)
+            if root.right is not None:
+                stack.append(root.right)
+            if root.left is not None:
+                stack.append(root.left)
+
+    return output
+
