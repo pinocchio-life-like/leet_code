971a09205e64c96299a25d04f221a1614df2fa9d
pinocchio-life-like <icbr19fl@gmail.com>
2023-12-26 15:33:41 +0300

    leet problems

--- solutions.py
+++ solutions.py
@@ -16,3 +16,29 @@ def sortedArrayToBST(nums):
     root.right = sortedArrayToBST(nums[mid+1:])
     
     return root
+
+
+
+def isBalanced(self, root):
+    """
+    :type root: TreeNode
+    :rtype: bool
+    """
+    def check_height(node):
+        if node is None:
+            return 0
+        
+        left_height = check_height(node.left)
+        if left_height == -1:
+            return -1
+        
+        right_height = check_height(node.right)
+        if right_height == -1:
+            return -1
+        
+        if abs(left_height - right_height) > 1:
+            return -1
+        
+        return max(left_height, right_height) + 1
+    
+    return check_height(root) != -1
