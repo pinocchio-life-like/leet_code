a155ddea5a00067ef642215b6c0d7a614213a2d7
pinocchio-life-like <icbr19fl@gmail.com>
2023-12-26 16:04:58 +0300

    leet problems

--- solutions.py
+++ solutions.py
@@ -95,3 +95,24 @@ def singleNumber(nums):
         result ^= num
     return result
 
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def hasCycle(head):
+    if head is None:
+        return False
+
+    slow = head
+    fast = head.next
+
+    while slow != fast:
+        if fast is None or fast.next is None:
+            return False
+
+        slow = slow.next
+        fast = fast.next.next
+
+    return True
+
